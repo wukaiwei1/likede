17c083fcb410e66f9b9948e05de3232ab25aae24
wukaiwei1 <2596862515@qq.com>
2022-07-30 19:47:30 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '立可得',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 头部是否固定
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 是否显示logo
+  sidebarLogo: true
+}
---
+++ src/views/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    登录页面
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  created() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang='less'>
+
+</style>
